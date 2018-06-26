9080bfe3d5defd1e809ddf5a43efd394b24f974e
Brad Schrag <brad.schrag@gmail.com>
2018-06-26 14:52:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-function theBeatlesPlay()
+function theBeatlesPlay(instruments, musicians) {
+  var comments = []
+  for (var i = 0; i < instruments.length; i++) {
+    comments.push(musicians[i] + ' plays ', instruments[i])
+  }
+  
+  return comments
+}
+
+function johnLennonFacts(jlFacts) {
+  var comments = []
+  for (var i = 0; i < jlFacts.length; i++) {
+    comments.push(facts[i] + '!!!')
+  }
+  
+  return comments
+}
+
+function iLoveTheBeatles(n) {
+  
+}
