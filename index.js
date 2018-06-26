912f98ead9d7c54fbf3b1d4fc13939e33a751989
Brad Schrag <brad.schrag@gmail.com>
2018-06-26 14:58:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ function johnLennonFacts(jlFacts) {
 function iLoveTheBeatles(n) {
   var loveBeatles = 'I love the Beatles!'
   var array = ['I love the Beatles!']
-  n++
+  
   do {
     array.push(loveBeatles)
     n++
   }
+  while (n<15)
   return array
 }
